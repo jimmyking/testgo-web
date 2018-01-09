b540d7ca6ef91dfe548f93bf0c70027279738190
ming.jin <ming.jin@kuailework.com>
2018-01-09 11:39:21 +0800

    登录页和主页面使用不同的视图

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,8 @@
 <template>
   <div id="app">
-    <router-view name="header"/>
+    <router-view />
   </div>
 </template>
-
-<script>
-export default {
-  name: 'app'
-}
-</script>
 
 <style>
 
--- src/router/router.js
+++ src/router/router.js
@@ -1,24 +1,19 @@
-import App from '../App'
-import header from '../components/header/header'
-
 const login = r => require.ensure([], () => r(require('../page/login/login')), 'login')
+const header = r => require.ensure([], () => r(require('../components/header/header')), 'header')
 const home = r => require.ensure([], () => r(require('../page/home/home')), 'home')
 
 export default [
   {
+    path: '/login',
+    component: login
+  },
+  {
     path: '/',
-    components: {
-      default: App,
-      header: header
-    },
+    component: header,
     children: [
       {
         path: '',
         redirect: '/home'
-      },
-      {
-        path: '/login',
-        component: login
       },
       {
         path: '/home',
